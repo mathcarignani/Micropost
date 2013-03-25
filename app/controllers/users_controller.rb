7101c370572882bfb310f943965396be40860ca5
Mathias <mathias@mathias-VirtualBox.(none)>
2013-03-25 19:25:22 -0300

    Cambios menores

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new # Creo el nuevo usuario, el que se va a utilizar en la vista para cargarle los datos
   end
 
+  def edit
+    @user = User.find(params[:id]) # Busco el usuario que tiene el id recibido por parametro
+  end
+
   def show
     @user = User.find(params[:id]) # Busco el usuario que tiene el id recibido por parametro
   end
@@ -18,4 +22,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  # Metodo para actualizar la informacion del usuario en la base de datos
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      # Si no se pudo actualizar el usuario
+      render 'edit'
+    end
+  end
+
 end
+
+# El new se utiliza para mostrar la pagina de nuevo, y al aceptar se utiliza el 
+# create, lo mismo pasa con el edit y el update
